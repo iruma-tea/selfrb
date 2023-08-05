0b97ff61fef608009201a2dd0f22ca125d24e1c2
iruma-tea <shusuke.think.pad@gmail.com>
2023-08-05 19:08:01 +0900

    5.2の学習完了

---
+++ chap05/str_encode.rb
@@ -0,0 +1,6 @@
+msg = '叱られて'
+
+encoded = msg.encode('Shift_JIS', undef: :replace, replace: '●')
+
+puts encoded
+puts encoded.encoding
---
+++ chap05/str_plus.rb
@@ -0,0 +1,6 @@
+msg = 'あいうえお'
+nmsg = msg + 'かきくけこ'
+
+puts msg
+puts nmsg
+puts msg.equal?(nmsg)
