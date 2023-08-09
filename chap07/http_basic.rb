c77d29fbdcd82acf97f085ddfa3efc179118e25a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-08-10 07:19:38 +0900

    リスト7.49まで学習完了

---
+++ chap07/http_basic.rb
@@ -0,0 +1,15 @@
+require "net/http"
+require "uri"
+
+uri = URI.parse("https://codezine.jp/")
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+res = http.get(uri.path)
+
+if res.code.to_i == 200
+  puts res.body
+else
+  puts "#{res.code}: #{res.message}"
+end
